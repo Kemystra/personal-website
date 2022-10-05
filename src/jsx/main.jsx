7599b2eed844fdaca5a175a1323988c74254f930
Izzminhal Akmal <izzmin97@gmail.com>
2022-10-05 17:21:00 +0800

    Added basic stuff

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,4 +1,11 @@
 const React = require("react");
-const ReactDOM = require("react-dom");
+const ReactDOM = require("react-dom/client");
 
-console.log("Is it working????");
+const Tile = props => {
+    return <div className="tile"></div>
+};
+
+var res = Array.from({ length: 100 }).map((e, index) => <Tile key={index}/>);
+
+const root = ReactDOM.createRoot(document.getElementById("background"));
+root.render(res);

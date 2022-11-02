e064493173e589327a915b2683c895e051c747b5
Izzminhal Akmal <izzmin97@gmail.com>
2022-11-02 14:53:32 +0800

    Removed old codes

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,11 +1,2 @@
 const React = require("react");
 const ReactDOM = require("react-dom/client");
-
-const Tile = props => {
-    return <div className="tile"></div>
-};
-
-var res = Array.from({ length: 100 }).map((e, index) => <Tile key={index}/>);
-
-const root = ReactDOM.createRoot(document.getElementById("background"));
-root.render(res);
